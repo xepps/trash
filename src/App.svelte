d98221bff6ae2c9233af21af25f10e1d5a70fc5a
Kristian Epps <kristian.epps@matillion.com>
2020-11-08 17:45:43 +0000

    first upgrade

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,38 @@
 <script>
+	import { onMount } from 'svelte'
 	import Notification from './Notification.svelte'
-import Progress from './Progress.svelte'
+	import Progress from './Progress.svelte'
+	import { formatNumber } from './utils'
 
 	const defaultZombieSpawnDuration = 500
+	let currentPopulation = 0
+	let zombies = 0
+	let zombieHelpers = false
+	let upgrades = false
+
 	let spawningZombie = false
-	let zombies = parseInt(localStorage.getItem('zombies'), 10) ?? 0
 	let currentSpawn = null
 	let autoZombies = false
 	let showSaved = false
 
-	$: currentZombieSpawnDuration = autoZombies ? 2 * defaultZombieSpawnDuration : defaultZombieSpawnDuration
+	onMount(() => {
+		const oldZombies = localStorage.getItem('zombies') ?? parseInt(localStorage.getItem('zombies'), 0)
+		const save = localStorage.getItem('save')
+		const game = save ? JSON.parse(save) : {}
 
+		currentPopulation = game?.currentPopulation ?? 7824071572
+		zombies = game?.zombies || oldZombies || 0
+		zombieHelpers = game?.zombieHelpers ?? false
+		upgrades = game?.upgrades ?? false
+	})
+
+	$: currentZombieSpawnDuration = autoZombies ? 2 * defaultZombieSpawnDuration : defaultZombieSpawnDuration
 
 	function createZombie () {
 		spawningZombie = true
 		currentSpawn = setTimeout(() => {
 			zombies += 1
+			currentPopulation -= 1
 			spawningZombie = false
 		}, currentZombieSpawnDuration)
 	}
@@ -25,8 +42,26 @@ import Progress from './Progress.svelte'
 		clearTimeout(currentSpawn)
 	}
 
+	function getUpgrade (upgrade) {
+		switch(upgrade.type) {
+			case 'upgrades':
+				upgrades = true
+				return
+			case 'zombieHelpers':
+				zombies -= upgrade.cost
+				zombieHelpers = true
+				return
+			default: return
+		}
+	}
+
 	function saveGame () {
-		localStorage.setItem('zombies', zombies)
+		localStorage.setItem('save', JSON.stringify({
+			currentPopulation,
+			zombies,
+			zombieHelpers,
+			upgrades
+		}))
 		showSaved = true
 		setTimeout(() => {
 			showSaved = false
@@ -36,6 +71,9 @@ import Progress from './Progress.svelte'
 	setInterval(() => {
 		if (autoZombies && !spawningZombie) {
 			createZombie()
+		}
+		if (zombies >= 10 && !upgrades) {
+			getUpgrade({ type: 'upgrades' })
 		}
 	}, 50)
 
@@ -47,15 +85,28 @@ import Progress from './Progress.svelte'
 
 <main>
 	<Notification show={showSaved} text="Saved" />
-	<h1>{zombies > 0 ? zombies : ''} {zombies === 1 ? 'Zombie' : 'Zombies'}</h1>
+	<h1>{zombies > 0 ? formatNumber(zombies) : ''} {zombies === 1 ? 'Zombie' : 'Zombies'}</h1>
+	{#if zombies}
+		<h2>and {formatNumber(currentPopulation)} humans</h2>
+	{/if}
 
 	<div class="row">
 		<button on:click={createZombie} disabled={spawningZombie}>
 			Construct Zombie
 		</button>
 		<Progress duration={currentZombieSpawnDuration} run={spawningZombie}/>
-		<label><input type="checkbox" bind:checked={autoZombies} on:change={clearSpawn}/> Auto zombies</label>
+		{#if zombieHelpers}
+			<label><input type="checkbox" bind:checked={autoZombies} on:change={clearSpawn}/> Auto zombies</label>
+		{/if}
 	</div>
+
+	{#if upgrades}
+		<div class="upgrades">
+			{#if !zombieHelpers}
+				<button on:click={() => getUpgrade({ type: 'zombieHelpers', cost: 20 })} disabled={zombies < 20}>Get Zombie Helpers (20 Zombies)</button>
+			{/if}
+		</div>
+	{/if}
 </main>
 
 <style>
